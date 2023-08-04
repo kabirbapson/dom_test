19e173c88a63284f024f8ebc178d81302072f208
Bappi Kabir <kabirbapson@gmail.com>
2023-08-04 23:33:20 +0100

    first init

--- seat_reservation/myscript.js
+++ seat_reservation/myscript.js
@@ -1,3 +1,43 @@
+// var html = "";
+// var counter = 1;
+
+// rows.forEach((row) => {
+//   html += `<div class='label'>${row}</div>`;
+//   for (let i = 0; i < 3; i++) {
+//     html += `<div id='${row + counter}'>${counter}</div>`;
+//     counter++;
+//   }
+//   counter=counter+12
+// });
+// document.getElementById("left").innerHTML = html;
+
+// html = ''
+// counter = 4
+
+// rows.forEach((row) => {
+//   // html += `<div class='label'>${row}</div>`;
+//   for (let i = 0; i < 9; i++) {
+//     html += `<div id='${row + counter}'>${counter}</div>`;
+//     counter++;
+//   }
+
+//   counter+=6
+// });
+// document.getElementById("middle").innerHTML = html;
+
+// html = ''
+// counter = 13
+
+// rows.forEach((row) => {
+
+//   for (let i = 0; i < 3; i++) {
+//     html += `<div id='${row + counter}'>${counter}</div>`;
+//     counter++;
+//   }
+//   html += `<div class='label'>${row}</div>`;
+//   counter+=12
+// });
+
 var rows = [
   "a",
   "b",
@@ -20,42 +60,28 @@ var rows = [
   "s",
   "t",
 ];
-var html = "";
 var counter = 1;
+function makeRows(place, loop, count) {
+  var html = "";
+  rows.forEach((row) => {
 
-rows.forEach((row) => {
-  html += `<div class='label'>${row}</div>`;
-  for (let i = 0; i < 3; i++) {
-    html += `<div id='${row + counter}'>${counter}</div>`;
-    counter++;
-  }
-  counter=counter+12
-});
-document.getElementById("left").innerHTML = html;
+    if (place == "left") {
+      html += `<div class='label'>${row}</div>`;
+    }
 
-html = ''
-counter=4
-rows.forEach((row) => {
-  // html += `<div class='label'>${row}</div>`;
-  for (let i = 0; i < 9; i++) {
-    html += `<div id='${row + counter}'>${counter}</div>`;
-    counter++;
-  }
-  
-  counter+=6
-});
-document.getElementById("middle").innerHTML = html;
+    for (let i = 0; i < loop; i++) {
+      html += `<div id='${row + counter}'>${counter}</div>`;
+      counter++;
+    }
+    counter += count;
+    
+    if (place === "right") {
+      html += `<div class='label'>${row}</div>`;
+    }
+  });
 
-html = ''
-counter=13
-rows.forEach((row) => {
-
-  for (let i = 0; i < 3; i++) {
-    html += `<div id='${row + counter}'>${counter}</div>`;
-    counter++;
-  }
-  html += `<div class='label'>${row}</div>`;
-  counter+=12
-});
-
-document.getElementById("right").innerHTML = html;
+  document.getElementById(place).innerHTML = html;
+}
+makeRows("left", 3, 12);
+makeRows("middle", 9, 6);
+makeRows("right", 3, 12);
