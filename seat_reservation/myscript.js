83766884b27e7204a57df4ed629e70bae7eee0ce
Bappi Kabir <kabirbapson@gmail.com>
2023-08-13 07:48:18 +0100

    first init

--- seat_reservation/myscript.js
+++ seat_reservation/myscript.js
@@ -185,7 +185,6 @@ function manageConfirmForm() {
 
   } else {
 
-    
     document.getElementById("confirmres").style.display = "none";
     document.getElementById("selectedseats").innerHTML = `You need to select some seats to reserve. <br> <a href='#' id='error'>Close</a> this dialog and pick at least one seat`
     
